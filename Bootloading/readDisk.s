33d2117dea3f39db9f412ee10b97c74422af3f41
MujuniReal <mujunireal@gmail.com>
2020-04-24 04:45:28 -0400

    edited readisk

--- Bootloading/readDisk.s
+++ Bootloading/readDisk.s
@@ -10,26 +10,37 @@ main:	jmp start
 //Fat formated with MBR not GPT
 
 #include "fatstruct.h"
+#include "printer.h"
 
 .func ReadSect
-// %al - number of sectors to read
-// %ah - SubFunction 2 from BIO interrupts
-// %ch - Low 8bits of Cylinder
-// %cl - High 2bits of cylinder, 6bits for sector
-// %dh - Head Number
-// %dl - Drive Number from BIOS
-// %es:%bx - Where read data is gonna be stored
+/*
+  %al - number of sectors to read
+  %ah - SubFunction 2 from BIO interrupts
+  %ch - Low 8bits of Cylinder
+  %cl - High 2bits of cylinder, 6bits for sector
+  %dh - Head Number
+  %dl - Drive Number from BIOS
+  %es:%bx - Where read data is gonna be stored
 
-// LBA
-// Sector = (LBA mod SectsPTrack) + 1
-// Head = (LBA / SectsPTrack) mod NumHeads
-// Cylinder = (LBA / SectsPTrack) / NumHeads
+  LBA
+  Sector = (LBA mod SectsPTrack) + 1
+  Head = (LBA / SectsPTrack) mod NumHeads
+  Cylinder = (LBA / SectsPTrack) / NumHeads
 
-//divide (dx:ax)/bx or any reg qoutient - %ax remainder - %dx
+  divide (dx:ax)/bx or any reg qoutient - %ax remainder - %dx
+  Before calling this guy set %cx to 4 so dat it attempts to try wen fail
+  we gonna use the cx register as our counter to count
+  4 3 more trys for disk reading
+  from the ax,bx,cx,dx function calling style i think
+  that this function is like so readSect(No_sects,*ptrSrc,*ptrDst)
+  This function expects LBA put in %ax register
+
+*/
 ReadSect:
-	//from the ax,bx,cx,dx function calling style i think
-	//that this function is like so readSect(No_sects,*ptrSrc,*ptrDst)
-	//Whatever register that bears the LBA put it in %ax
+	push %ax
+	push %cx
+	push %bx
+
 	//Sector
 	xor %bx,%bx
 	xor %dx,%dx
@@ -50,11 +61,32 @@ ReadSect:
 
 	//finalyy
 	mov LogDrvNo,%dl
-	mov $0x2,%ah
+	mov $0x0201,%ax		//We intend to (read) (one) Sector
 	pop %bx
 	int $0x13
-	//jc FailedToread
-	
+	jc FailedToread
+	lea (SucReadStr),%si
+	call PrintIt
+	jmp exitRead
+
+
+FailedToread:			//This function attempts to try reading
+	pop %cx			//the sector 3 more times 3more trials
+	xor %ax,%ax
+	int $0x13		//Reset the disk and we try again
+	pop %ax			//Put Back LBA in the mighty register
+	dec %cx
+	cmp $0x0,%cx
+	jnz ReadSect		//Totally failed
+	lea (FailTRStr),%si	//Totally failed to read string
+	call PrintIt
+
+exitRead:
+	pop %cx
+	pop %ax
+	retw
+
+		
 .endfunc
 
 
