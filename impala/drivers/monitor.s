678a915a4c0b7d2db9e62e8899fab748d2baa044
MujuniReal <mujunireal@gmail.com>
2021-07-01 16:41:43 +0300

    clears clearscreen working

--- impala/drivers/monitor.s
+++ impala/drivers/monitor.s
@@ -7,6 +7,8 @@
 #define DEFAULT_ATTRIBUTE 0x0f      /* white on black, ie higher 4bits->background, lower 4bits-> text/cursor color */
 #define SPACEBAR 0x20
 #define ATTRIB_SPACE 0x0f20         /* attribute and character 2bytes */
+#define CURSOROFFSET_HI 14
+#define CURSOROFFSET_LO 15
 .code32
 .text
 .global clears
@@ -17,22 +19,33 @@ clears:
     xor %eax,%eax
     push $TOTAL_PIXEL
     movw $ATTRIB_SPACE,%ax   /* fill screen with spaces spaces is 0x20 thus clearing screen*/                       
-    push %eax  
+    push %eax 
     push $VGA_ADDRESS
     call memsetw
     add $0xc,%esp
 
     movw $0x0000,(cursr_pos)               /* After clearing screen, set cursor to 0,0 top left */
     call setcursor
-    pop
     ret
 
 //void setcursor();
 setcursor:
-    movw cursr_pos,%ax
-    out %ah,$VIDEO_CMD_PORT         /* high byte for cursor location */
-    out %al,$VIDEO_DATA_PORT        /* low byte for cursor location */
-    ret                                /* cursor location y = overall location / 80, x = overall location mod 80 */
+    movb $CURSOROFFSET_HI,%al
+    movw $VIDEO_CMD_PORT,%dx
+    outb %al,(%dx)
+    movw (cursr_pos),%ax		   
+    xchg %ah,%ah
+    movw $VIDEO_DATA_PORT,%dx
+    outb %al,(%dx)			  /* high byte for cursor location */
+
+    movb $CURSOROFFSET_LO,%al
+    movw $VIDEO_CMD_PORT,%dx
+    outb %al,(%dx)
+    movw (cursr_pos),%ax
+    movw $VIDEO_DATA_PORT,%dx		 
+    outb %al,(%dx)			  /* low byte for cursor location */
+/* cursor location y = overall location / 80, x = overall location mod 80 */
+    ret
 
 
 
--- impala/main.c
+++ impala/main.c
@@ -1,6 +1,7 @@
 #include "include/system.h"
 #include "include/irq.h"
 
+extern void clears();
 
 void run_impala(){
     __asm__("cli");
@@ -10,6 +11,14 @@ void run_impala(){
     install_irqs();
 
     __asm__("sti");
+
+    memsetw((uint16*)0xb8000,(uint16)0x720,80*25);
+
+    //clears();
+    outportb(14,0x3d4);
+    outportb(0x0,0x3d5);
+    outportb(15,0x3d4);
+    outportb(0x0,0x3d5);
 
 hang:
     goto hang;
