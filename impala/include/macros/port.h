116daa1275867ba81216c6853575bb6cd8d74b96
MujuniReal <mujunireal@gmail.com>
2021-10-19 19:17:32 +0300

    diskread in 32bit mode 40%

--- impala/include/macros/port.h
+++ impala/include/macros/port.h
@@ -1,10 +1,12 @@
 /* This macro is to handle port activities */
 /* save your contents of %eax somewhere before initiating these macros */
 
+
 .macro outportb data port
-	mov $\data,%al
-	outb %al,$\port
+mov $\data,%al
+    outb %al,$\port
 .endm
+
 
 .macro inportb port
 	inb $\port,%al
