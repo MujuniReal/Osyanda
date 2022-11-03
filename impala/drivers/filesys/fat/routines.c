114718cac1cb8242804e0a2190fa8340e5bf8267
MujuniReal <mujunireal@gmail.com>
2022-11-03 07:36:51 +0300

    routines

--- impala/drivers/filesys/fat/routines.c
+++ impala/drivers/filesys/fat/routines.c
@@ -10,10 +10,35 @@ extern void rdsk(void *buf, int size);
 
 typedef struct dirEntry dirEntryStruct;
 
-short int fat_tab[65536];
+uint16 fat_tab[65536];
 dirEntryStruct rootdir_mem[MAXROOTENTRIES];
 uint32 rootdir_start;
 uint32 firstDataClust;
+
+
+struct fat16bpb {
+  char devOEM[8];
+  uint16 byPsect;
+  uint8 sectPclust;
+  uint16 resSects;
+  uint8 fatTabs;
+  uint16 nRootDirEs;
+  uint16 totSects;
+  uint16 medDescr;
+  uint16 sectsPfat;
+  uint16 sectsPtrck;
+  uint16 nHeads;
+  uint16 nHiddenSects;
+  uint16 nHiddenSectsHi;
+  uint32 totSectsFs;
+  uint8 drvNo;
+  uint8 resrvd;
+  uint8 extSig;
+  uint32 drvSerNum;
+  char volLabel[11];
+  char fsType[8];
+  
+}__attribute__((packed));
 
 void readfat(void){
 
@@ -43,5 +68,4 @@ uint32 find_file_startcluster(char *filename){
 
 void *readfatfile(uint32 startClust, void *buf, uint32 size){
   //This function sets the firstDataClust variable
-
 }
