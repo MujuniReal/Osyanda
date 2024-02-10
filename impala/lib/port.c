a27a4f01136d24bcd82fcf87ede1e213dbd5f753
MujuniReal <mujunireal@gmail.com>
2024-02-10 11:52:37 +0300

    Glory to God !!!!!!!!!!

--- impala/lib/port.c
+++ impala/lib/port.c
@@ -1,19 +1,19 @@
 #include <types.h>
 
+void outportb(uint8 data, uint16 port)
+{
 
-void outportb(uint8 data, uint16 port){
-  
-  asm("outb %%al,(%%dx)"::"a"(data), "dN"(port));
-  
+  asm("outb %%al,(%%dx)" ::"a"(data), "dN"(port));
 }
 
-uint8 inportb(uint16 port){ 
+uint8 inportb(uint16 port)
+{
 
-  uint8 result; 
-  asm("mov %%ax,%%dx; inb (%%dx)":"=a"(result):"a"(port));
-  return result; 
-} 
+  uint8 result;
+  asm("mov %%ax,%%dx; inb (%%dx)" : "=a"(result) : "a"(port));
+  return result;
+}
 
-inportw(){}
+inportw() {}
 
-outportw(){}
+outportw() {}
